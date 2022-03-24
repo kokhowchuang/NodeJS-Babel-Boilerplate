b93ddfbd766eaece1e5f85ce9ff88c2a408313b4
kokhow <[chuangkokhow@gmail.com]>
2022-03-24 22:10:09 +0800

    trim unwanted tests

--- app/controllers/sample.js
+++ app/controllers/sample.js
@@ -1,27 +1,11 @@
 "use strict";
 
-import path from "path";
-import jwt from "jsonwebtoken";
-import aws from "aws-sdk";
-import multer from "multer";
-import multerS3 from "multer-s3";
 import mongoose from "mongoose";
 import { variable } from "../config/environment_variable";
 import { logger } from "../config/logger";
 
-import User from "../models/user";
-import SubscribedUser from "../models/subscribed_user";
-import * as PostController from "../controllers/post_controller";
-
+import User from "../models/sample";
 const env = process.env.NODE_ENV || "development";
-const jwtSecretKey = process.env.JWT_SECRET_KEY || variable[env].JWT.SECRETKEY;
-
-const s3 = new aws.S3({
-  accessKeyId: process.env.S3_ACCESS_KEY_ID || variable[env].S3.ACCESS_KEY_ID,
-  secretAccessKey:
-    process.env.S3_SECRET_ACCESS_KEY || variable[env].S3.SECRET_ACCESS_KEY,
-  region: process.env.S3_REGION || variable[env].S3.REGION,
-});
 
 /**
  * Get user details
